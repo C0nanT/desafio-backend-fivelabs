2e03950edad9d46dfc718d9709c6ebd9b986d42c
Conan Torres <conantorreswf@gmail.com>
2025-04-23 20:19:09 -0300

    feat: adiciona TagsTasks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(TagsSeeder::class);
         $this->call(TaskSeeder::class);
+        $this->call(TagsTasksSeeder::class);
     }
 }
---
+++ database/seeders/TagsTasksSeeder.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TagsTasks;
+use App\Models\Tasks;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TagsTasksSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('tags_tasks')->truncate();
+
+        $tags_tasks = [
+            [
+                'tag_id' => 1,
+                'task_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'tag_id' => 2,
+                'task_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'tag_id' => 3,
+                'task_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'tag_id' => 4,
+                'task_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'tag_id' => 5,
+                'task_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'tag_id' => 6,
+                'task_id' => 3,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'tag_id' => 7,
+                'task_id' => 3,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'tag_id' => 8,
+                'task_id' => 4,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        TagsTasks::insert($tags_tasks);
+    }
+}
